6045e710d61023cc5022a00a96b6ea3cba90764c
Lucy Ouma <akinyilucy09@gmail.com>
2024-03-22 17:53:46 -0700

    0x0A. Configuration management Shell code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # This code will install a specified version of flask (2.1.0)
-
 exec { 'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
+
