cc35738dc3b404c6b8882462caed8845b8ee7322
Lucy Ouma <akinyilucy09@gmail.com>
2024-08-10 18:56:44 -0700

    added gitignore

--- 0x14-mysql/3-db_setup.sql
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-creates the replica user
-CREATE USER IF NOT EXISTS
-	'replica_user'@'%'
-	IDENTIFIED BY 'replica_user_pwd';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
-GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
