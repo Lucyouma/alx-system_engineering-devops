0b507566651308912eb83a77252c550a8f3303d3
Lucyouma <akinyilucy09@gmail.com>
2024-09-12 17:43:59 -0400

    added exception check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,11 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    try:
+
+        response = requests.get(url, headers=headers, allow_redirects=False)
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
-    else:
+    except Exception as e:
         return 0
