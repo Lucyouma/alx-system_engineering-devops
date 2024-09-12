6b9d5d4d335e8eb5e3273964e7f4e86367b665a8
Lucyouma <akinyilucy09@gmail.com>
2024-09-12 17:48:00 -0400

    changed user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "My User Agent 5.0"}
     try:
 
         response = requests.get(url, headers=headers, allow_redirects=False)
