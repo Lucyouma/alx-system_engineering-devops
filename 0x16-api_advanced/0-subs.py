a92a8b8e81465089807b830343897b16d620f795
Lucyouma <akinyilucy09@gmail.com>
2024-09-12 17:54:01 -0400

    added status code check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,15 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "My User Agent 5.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     try:
 
         response = requests.get(url, headers=headers, allow_redirects=False)
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0
     except Exception as e:
         return 0
